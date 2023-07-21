6e3ba506809cdb303fe5e186fff99def70f6513f
Paralikar5 <89960040+Paralikar5@users.noreply.github.com>
2023-07-21 11:00:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/trippy-self-project" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/service" element={<Service />} />
         <Route path="/contact" element={<Contact />} />
